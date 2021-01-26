ebeb153f2ddf9a3215fea85f1cd695fa4c6e1874
Gavin6581 <3122655867@qq.com>
2021-01-26 11:48:23 +0800

    连接mysql数据库登录操作

--- src/main/java/com/gavin/community/controller/AuthorizeController.java
+++ src/main/java/com/gavin/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.gavin.community.controller;
 
 import com.gavin.community.dto.AccessTokenDto;
 import com.gavin.community.dto.GithubUser;
+import com.gavin.community.mapper.UserMapper;
+import com.gavin.community.model.User;
 import com.gavin.community.provider.GithubProvicer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvicer githubProvicer;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -24,7 +32,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
@@ -32,8 +41,22 @@ public class AuthorizeController {
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvicer.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvicer.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvicer.getUser(accessToken);
+
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis()); //当前时间
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功 写session和cookie
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败 重新登录
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/gavin/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.gavin.community.mapper;
+
+import com.gavin.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into guser (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+}
